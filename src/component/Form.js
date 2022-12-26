b41a91e7d6338affc769855057190233a4e2a8c9
M.Haroon Ali <haroon.ali@invozone.com>
2022-12-27 01:02:44 +0500

    resolve issues

--- src/component/Form.js
+++ src/component/Form.js
@@ -1,49 +1,51 @@
 import React from "react";
 import { useState } from "react";
 
-const Form = () =>{
-    const[inputs, setInputs] = useState({});
-    const [myCar, setMyCar] = useState("Volvo");
-    const handleChange = (event) => {
-        const name = event.target.name;
-        // eslint-disable-next-line no-unused-vars
-        const value = event.target.value;
-        setInputs(value => ({...value , [name] : value}));
-    };
-    const handleCar= (event) => {
-        setMyCar(event.target.value);
-      };
-    const handleSubmit = (event) =>{
-        event.preventDefault();
-        alert(inputs);
-    };
+const Form = () => {
+  const [inputs, setInputs] = useState({});
+  const [myCar, setMyCar] = useState("Volvo");
+  const handleChange = (event) => {
+    const name = event.target.name;
+    // eslint-disable-next-line no-unused-vars
+    const value = event.target.value;
+    setInputs((value) => ({ ...value, [name]: value }));
+  };
+  const handleCar = (event) => {
+    setMyCar(event.target.value);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(inputs);
+  };
 
-    return (
-        <form onSubmit={handleSubmit}>
-          <label>Enter your name:
-          <input 
-                type="text" 
-                name="username" 
-                value={inputs.username || ""} 
-                onChange={handleChange}
-            />
-          </label>
-          <label>Enter your age:
-            <input 
-                type="number" 
-                name="age" 
-                value={inputs.age || ""} 
-                onChange={handleChange}
-            />
-        </label>
-        <select value={myCar} onChange={handleCar}>
-            <option value="Ford">Ford</option>
-            <option value="Volvo">Volvo</option>
-            <option value="Fiat">Fiat</option>
-        </select>
-          <input type = "submit"/>
-        </form>
-      );
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Enter your name:
+        <input
+          type="text"
+          name="username"
+          value={inputs.username || ""}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Enter your age:
+        <input
+          type="number"
+          name="age"
+          value={inputs.age || ""}
+          onChange={handleChange}
+        />
+      </label>
+      <select value={myCar} onChange={handleCar}>
+        <option value="Ford">Ford</option>
+        <option value="Volvo">Volvo</option>
+        <option value="Fiat">Fiat</option>
+      </select>
+      <input type="submit" />
+    </form>
+  );
 };
 
 export default Form;
--- src/component/fetchDummyData.js
+++ src/component/fetchDummyData.js
@@ -14,7 +14,7 @@ const FetchDummyData = () => {
                     })
             }
         </>
-    )
-}
+    );
+};
 
 export default FetchDummyData;
